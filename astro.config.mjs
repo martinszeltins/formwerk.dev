a0e434329dae58a4d39cda8b50a333ec1f285850
Abdelrahman Awad <logaretm1@gmail.com>
2024-08-31 04:58:13 +0300

    chore: added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -31,5 +33,6 @@ export default defineConfig({
         },
       ],
     }),
+    tailwind(),
   ],
 });
