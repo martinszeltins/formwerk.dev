aa7d9327c600b902a88a97abf4c058b6d938f2f3
Abdelrahman Awad <logaretm1@gmail.com>
2024-09-07 06:40:08 +0300

    feat: document output anatomy

--- src/components/AnatomySlider.vue
+++ src/components/AnatomySlider.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="flex w-full max-w-xs flex-col items-center gap-1">
-    <AnatomyPart text="Label" position="top" :distance="20" class="mr-auto">
-      <span>Label</span>
-    </AnatomyPart>
+    <div class="flex w-full items-center">
+      <AnatomyPart text="Label" position="top" :distance="20" class="mr-auto">
+        <span>Label</span>
+      </AnatomyPart>
+
+      <AnatomyPart text="Output" position="right" class="mr-6" :distance="20">
+        <span>74</span>
+      </AnatomyPart>
+    </div>
 
     <AnatomyPart text="Group" class="w-full">
       <div
-        class="relative flex w-full items-center rounded border border-dashed border-zinc-500 pl-6"
+        class="relative flex w-full items-center rounded border border-dashed border-zinc-500 px-3 py-4"
       >
         <AnatomyPart
           text="Track"
           position="bottom span-right"
-          :distance="20"
+          :distance="30"
           class="relative flex w-full items-center"
         >
           <div class="h-0.5 w-full rounded-full bg-zinc-500"></div>
         </AnatomyPart>
 
         <div
-          class="flex -translate-x-[5cqw] translate-y-1 items-center justify-center"
+          class="absolute right-1/4 flex translate-y-1 items-center justify-center"
         >
           <AnatomyPart text="Thumb" position="center" mirrored>
             <PhCircleFill class="h-6 w-6 text-zinc-400" />
