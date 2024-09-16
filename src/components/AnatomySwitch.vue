2d12175c66bf1c0b259a95157938125eb0f0ac85
Abdelrahman Awad <logaretm1@gmail.com>
2024-09-16 03:44:33 +0300

    feat: switches guide

--- src/components/AnatomySwitch.vue
+++ src/components/AnatomySwitch.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="flex w-full max-w-xs items-center justify-center gap-4">
-    <AnatomyPart
-      text="Input"
-      position="left"
-      class="flex items-start justify-center"
-    >
-      <PhToggleLeftFill class="h-12 w-12 text-zinc-600" />
-    </AnatomyPart>
+  <div class="flex w-full max-w-xs flex-col items-center justify-center">
+    <div class="flex items-center gap-4">
+      <AnatomyPart
+        text="Input"
+        position="left"
+        class="flex items-start justify-center"
+      >
+        <PhToggleLeftFill class="h-12 w-12 text-zinc-600" />
+      </AnatomyPart>
 
-    <AnatomyPart text="Label" position="right">
-      <span>Label</span>
+      <AnatomyPart text="Label" position="right">
+        <span>Label</span>
+      </AnatomyPart>
+    </div>
+
+    <AnatomyPart text="Error Message" position="left">
+      <span class="text-sm">Something went wrong</span>
     </AnatomyPart>
   </div>
 </template>
